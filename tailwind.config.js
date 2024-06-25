7c1f57f351117b4670fef643bb87c91edb07bf5a
Al Faisal <54954828+alfaisal1231@users.noreply.github.com>
2024-06-26 01:53:05 +0700

    V0.1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: { 
+      fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    },
+  },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [      
+    // require('@tailwindcss/aspect-ratio'),
+  ],
+}
